83b9fa31142960de65fa17473af27d2ade7f135d
zhaowenkai <799480165@qq.com>
2023-05-18 13:48:48 +0800

    add component example

---
+++ examples/Examples.Component/Components/Demo/Style.cs
@@ -0,0 +1,42 @@
+﻿using CssInCs;
+
+namespace Examples.Component
+{
+    public partial class Demo
+    {
+        // or component id,name etc.
+        private const string Name = "demo";
+        private const string ClassName = $".{Name}";
+
+        private CSSObject GenStyle(CssToken token)
+        {
+            return new CSSObject
+            {
+                [$"{ClassName}"] = new ()
+                {
+                    Width = $"{Width}px",
+                    MinHeight = "300px",
+                    Display = "flex",
+                    FlexDirection = "row",
+                    FlexWrap = "wrap",
+                    JustifyContent = "space-between",
+                    ["& .item"] = new ()
+                    {
+                        Width = "80px",
+                        Height = "100px",
+                        Border = "1px solid #DDD",
+                        TextAlign = "center",
+                        Display = "flex",
+                        AlignItems = "center",
+                        JustifyContent = "center"
+                    }
+                }
+            };
+        }
+
+        protected override CSSObject[] UseStyle(CssToken token) => new []
+        {
+            GenStyle(token)
+        };
+    }
+}
