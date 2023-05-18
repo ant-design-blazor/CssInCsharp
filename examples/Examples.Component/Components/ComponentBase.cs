9018938bc9b74f91eb0315661311b55a00c9fe10
zhaowenkai <799480165@qq.com>
2023-05-18 13:48:48 +0800

    add component example

---
+++ examples/Examples.Component/Components/ComponentBase.cs
@@ -0,0 +1,34 @@
+﻿using CssInCs;
+using Microsoft.AspNetCore.Components;
+
+namespace Examples.Component
+{
+    public class CssToken
+    {
+        // some css token, eg: theme parameters
+        public string ThemeColor { get; set; }
+        public string ThemeBackground { get; set; }
+    }
+
+    public abstract class AntdComponentBase: ComponentBase
+    {
+        public RenderFragment WrapSSR(RenderFragment dom)
+        {
+            var token = new CssToken();
+            var styleList = UseStyle(token);
+            return (builder) =>
+            {
+                var i = 0;
+                builder.AddContent(i++, dom);
+                builder.OpenElement(i++, "style");
+                foreach (var style in styleList)
+                {
+                    builder.AddContent(i++, style.ToString());
+                }
+                builder.CloseElement();
+            };
+        }
+
+        protected abstract CSSObject[] UseStyle(CssToken token);
+    }
+}
