b09e2ac7860525348472467e20724ad2bbef3564
zhaowenkai <799480165@qq.com>
2023-12-01 18:46:11 +0800

    update package info

--- src/Styles/StyleOutlet.cs
+++ src/Styles/StyleOutlet.cs
@@ -9,21 +9,27 @@ namespace CssInCs
     {
         internal const string StyeSectionOutletName = "cssincs_style";
 
+        /*
+         * using SectionOutlet to render style tag
+         * note:
+         * Since only one head::after can exist,
+         * Therefore, the HeadOutlet needs to be placed here.
+         * The order of HeadOutlet needs to be placed after StyleSection.
+         */
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
-            // useing SectionOutlet to render style tag
+#if NET8_0_OR_GREATER
             builder.OpenComponent<SectionOutlet>(0);
             builder.AddAttribute(1, nameof(SectionOutlet.SectionName), StyeSectionOutletName);
             builder.CloseComponent();
-
-            /*
-             * note:
-             * Since only one head::after can exist,
-             * Therefore, the HeadOutlet needs to be placed here for rendering.
-             * The order of HeadOutlet needs to be placed after StyleSection.
-             */
             builder.OpenComponent<HeadOutlet>(2);
             builder.CloseComponent();
+#elif NET6_0_OR_GREATER
+            builder.OpenComponent<HeadOutlet>(2);
+            builder.CloseComponent();
+#else
+#warning "The TargetFramework does not support the StyleOutlet component, upgrade the TargetFramework or use Style component only."
+#endif
         }
     }
 }
