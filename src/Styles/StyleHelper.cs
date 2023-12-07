fa4dab4e3ede216202306f270fbe448b1a08e5aa
zhaowenkai <799480165@qq.com>
2023-12-07 10:19:17 +0800

    fix the accuracy of number type mapping

--- src/Styles/StyleHelper.cs
+++ src/Styles/StyleHelper.cs
@@ -40,5 +40,15 @@ namespace CssInCSharp
                 builder.CloseComponent();
             };
         }
+
+        public static RenderFragment UseStyleRegister(StyleInfo style)
+        {
+            StyleOutlet.Register(style);
+            return builder =>
+            {
+                builder.OpenComponent<StyleContent>(0);
+                builder.CloseComponent();
+            };
+        }
     }
 }
