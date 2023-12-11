79f09b6a6dfcf792aaab083f9446e62c8a337780
zhaowenkai <799480165@qq.com>
2023-12-11 15:29:16 +0800

    add internal style content component

--- src/Styles/StyleHelper.cs
+++ src/Styles/StyleHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using System;
+using System.Collections.Concurrent;
 
 namespace CssInCSharp
 {
@@ -13,22 +14,29 @@ namespace CssInCSharp
 
     public static class StyleHelper
     {
-        public static RenderFragment UseStyleRegister(StyleInfo[] styles)
-        {
-            StyleOutlet.Register(styles);
-            return builder =>
-            {
-                builder.OpenComponent<StyleContent>(0);
-                builder.CloseComponent();
-            };
-        }
+        // todo: how to UnRegister style, if compoent disposed.
+        internal static ConcurrentDictionary<string, StyleInfo> Styles = new ConcurrentDictionary<string, StyleInfo>();
 
         public static RenderFragment UseStyleRegister(StyleInfo style)
         {
-            StyleOutlet.Register(style);
+            var path = $"{style.TokenKey}|{string.Join("|", style.Path)}";
+            Styles.TryAdd(path, style);
             return builder =>
             {
-                builder.OpenComponent<StyleContent>(0);
+                builder.OpenComponent<StyleContentInternal>(0);
+                builder.AddAttribute(1, "ChildContent", (RenderFragment)((child) =>
+                {
+                    var i = 0;
+                    foreach (var item in Styles)
+                    {
+                        child.OpenComponent<Style>(i++);
+                        child.AddAttribute(i++, "HashId", item.Value.HashId);
+                        child.AddAttribute(i++, "TokenKey", item.Value.TokenKey);
+                        child.AddAttribute(i++, "Path", item.Key);
+                        child.AddAttribute(i++, "StyleFn", item.Value.StyleFn);
+                        child.CloseComponent();
+                    }
+                }));
                 builder.CloseComponent();
             };
         }
--- src/Styles/StyleOutlet.cs
+++ src/Styles/StyleOutlet.cs
@@ -4,28 +4,13 @@ using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Sections;
 #endif
 using Microsoft.AspNetCore.Components.Web;
-using System.Collections.Concurrent;
 
 namespace CssInCSharp
 {
     public sealed class StyleOutlet : ComponentBase
     {
-        internal const string StyeSectionOutletName = "cssincs_style";
-        internal static ConcurrentDictionary<string, StyleInfo> Styles = new ConcurrentDictionary<string, StyleInfo>();
-        internal static void Register(StyleInfo[] styles)
-        {
-            foreach(var style in styles)
-            {
-                var path = $"{style.TokenKey}|{string.Join("|", style.Path)}";
-                Styles.TryAdd(path, style);
-            }
-        }
-
-        internal static void Register(StyleInfo style)
-        {
-            var path = $"{style.TokenKey}|{string.Join("|", style.Path)}";
-            Styles.TryAdd(path, style);
-        }
+        internal const string StyeSectionOutletName = "cssincsharp_style";
+        internal const string InternalStyeSectionOutletName = "cssincsharp_style_internal";
 
         /*
          * using SectionOutlet to render style tag
@@ -37,19 +22,16 @@ namespace CssInCSharp
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
             var i = 0;
-            foreach (var item in Styles)
-            {
-                builder.OpenComponent<Style>(i++);
-                builder.AddAttribute(i++, "HashId", item.Value.HashId);
-                builder.AddAttribute(i++, "TokenKey", item.Value.TokenKey);
-                builder.AddAttribute(i++, "Path", item.Key);
-                builder.AddAttribute(i++, "StyleFn", item.Value.StyleFn);
-                builder.CloseComponent();
-            }
 #if NET8_0_OR_GREATER
+            // internal style content for components
+            builder.OpenComponent<SectionOutlet>(i++);
+            builder.AddAttribute(i++, nameof(SectionOutlet.SectionName), InternalStyeSectionOutletName);
+            builder.CloseComponent();
+            // style content for pages
             builder.OpenComponent<SectionOutlet>(i++);
             builder.AddAttribute(i++, nameof(SectionOutlet.SectionName), StyeSectionOutletName);
             builder.CloseComponent();
+            // original head
             builder.OpenComponent<HeadOutlet>(i++);
             builder.CloseComponent();
 #elif NET6_0_OR_GREATER
